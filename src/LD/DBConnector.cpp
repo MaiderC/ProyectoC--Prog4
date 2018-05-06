c6ad64eb4aa1d4570ac4c338dd21e79c2e080a25
maiderc <maiderc@opendeusto.es>
2018-05-06 09:18:30 +0200

    changes done in DBConnector

--- src/LD/DBConnector.cpp
+++ src/LD/DBConnector.cpp
@@ -178,7 +178,7 @@
 	      std::cout << sqlite3_errmsg(db) << std::endl;
 	      return result;
 	    }
-	    std::string nick= jugadorInsertar.nick;
+	    std::string nick= jugadorInsertar.getNick();
 	    //Le pasamos el nick al statement
 	    result = sqlite3_bind_text(stmt, 1, nick.c_str(), nick.length(), SQLITE_STATIC);
 
@@ -233,7 +233,7 @@ int DBConnector::insert_Pregunta (Pregunta_Respuestas PreguntaInsertar)
 
 	    	//ID
 
-	    	result = sqlite3_bind_int(stmt, 1, PreguntaInsertar.ID);
+	    	result = sqlite3_bind_int(stmt, 1, PreguntaInsertar.getID());
 	    	if (result != SQLITE_OK)
 		    {
 		      std::cout << "Error binding parameters" << std::endl;
@@ -243,7 +243,7 @@ int DBConnector::insert_Pregunta (Pregunta_Respuestas PreguntaInsertar)
 		    }
 
 		    //PREGUNTA
-		    std::string pregunta = PreguntaInsertar.pregunta;
+		    std::string pregunta = PreguntaInsertar.getPregunta();
 	
 		    result = sqlite3_bind_text(stmt, 2, pregunta.c_str(), pregunta.length(), SQLITE_STATIC);
 		    if (result != SQLITE_OK)
@@ -256,7 +256,7 @@ int DBConnector::insert_Pregunta (Pregunta_Respuestas PreguntaInsertar)
 
 
 		    //R1:
-		    std::string r1 = PreguntaInsertar.Respuesta1;
+		    std::string r1 = PreguntaInsertar.getRespuesta1();
 	
 		    result = sqlite3_bind_text(stmt, 3, r1.c_str(), r1.length(), SQLITE_STATIC);
 		    if (result != SQLITE_OK)
@@ -268,7 +268,7 @@ int DBConnector::insert_Pregunta (Pregunta_Respuestas PreguntaInsertar)
 		    }
 
 		    //R2:
-		    std::string r2 = PreguntaInsertar.Respuesta2;
+		    std::string r2 = PreguntaInsertar.getRespuesta2();
 
 		    result = sqlite3_bind_text(stmt, 4, r2.c_str(), r2.length(), SQLITE_STATIC);
 		    if (result != SQLITE_OK)
@@ -281,9 +281,9 @@ int DBConnector::insert_Pregunta (Pregunta_Respuestas PreguntaInsertar)
 
 		    //SOLO EN MEDIA O DIFÍCIL
 		    //R3:
-		    if (PreguntaInsertar.Dificultad != "#")
+		    if (PreguntaInsertar.getDificultad() != "#")
 		    {
-			    std::string r3 = PreguntaInsertar.Respuesta3;
+			    std::string r3 = PreguntaInsertar.getRespuesta3();
 			    
 			    result = sqlite3_bind_text(stmt, 5, r3.c_str(), r3.length(), SQLITE_STATIC);
 			    if (result != SQLITE_OK)
@@ -309,9 +309,9 @@ int DBConnector::insert_Pregunta (Pregunta_Respuestas PreguntaInsertar)
 
 		    //SOLO EN DIFÍCIL
 		    //R4:
-		    if (PreguntaInsertar.Dificultad == "###")
+		    if (PreguntaInsertar.getDificultad() == "###")
 		    {
-			    std::string r4 = PreguntaInsertar.Respuesta4;
+			    std::string r4 = PreguntaInsertar.getRespuesta4();
 			    
 			    result = sqlite3_bind_text(stmt, 6, r4.c_str(), r4.length(), SQLITE_STATIC);
 			    if (result != SQLITE_OK)
@@ -336,7 +336,7 @@ int DBConnector::insert_Pregunta (Pregunta_Respuestas PreguntaInsertar)
 		    }
 
 		    //DIFICULTAD
-		    std::string dificultad = PreguntaInsertar.Dificultad;
+		    std::string dificultad = PreguntaInsertar.getDificultad();
 	
 		    result = sqlite3_bind_text(stmt, 7, dificultad.c_str(), dificultad.length(), SQLITE_STATIC);
 		    if (result != SQLITE_OK)
@@ -380,8 +380,8 @@ int DBConnector::update_Jugador(jugador jugadorModificar)//Pasar el jugador comp
 		sqlite3_stmt *stmt;
 		char sql[] = "update Jugadores set PUNTUACION = ? where NICK = ?";
 	    
-	    std::string nick = jugadorModificar.nick;
-	    int puntuacion = jugadorModificar.puntuacion;
+	    std::string nick = jugadorModificar.getNick();
+	    int puntuacion = jugadorModificar.getPuntuacion();
 	    //Preparar el statement:
 	    int result = sqlite3_prepare_v2(db, sql, strlen(sql) + 1, &stmt, NULL) ;
 	    if (result != SQLITE_OK) 
@@ -442,12 +442,12 @@ int DBConnector::update_Jugador(Pregunta_Respuestas preguntaModificar)
 		sqlite3_stmt *stmt;
 		char sql[] = "update Preguntas set PREGUNTA = ?, R1 = ?, R2 = ?, R3 = ?, R4 = ?, DIFICULTAD = ? where ID = ?";
 	    
-	    std::string pregunta = preguntaModificar.Pregunta;
-	    std::string r1 = preguntaModificar.Respuesta1;
-	    std::string r2 = preguntaModificar.Respuesta2;
-	    std::string r3 = preguntaModificar.Respuesta3;
-	    std::string r4 = preguntaModificar.Respuesta4;
-	    std::string dificultad = preguntaModificar.dificultad;
+	    std::string pregunta = preguntaModificar.getPregunta();
+	    std::string r1 = preguntaModificar.getRespuesta1();
+	    std::string r2 = preguntaModificar.getRespuesta2();
+	    std::string r3 = preguntaModificar.getRespuesta3();
+	    std::string r4 = preguntaModificar.getRespuesta4();
+	    std::string dificultad = preguntaModificar.getDificultad();
 	    int ID = preguntaModificar.ID;
 
 	    //Preparar el statement:
@@ -526,7 +526,7 @@ int DBConnector::update_Jugador(Pregunta_Respuestas preguntaModificar)
 
 		    //SOLO EN DIFÍCIL
 		    //R4:
-		    if (PreguntaInsertar.Dificultad == "###")
+		    if (dificultad == "###")
 		    {
 			    
 			    result = sqlite3_bind_text(stmt, 5, r4.c_str(), r4.length(), SQLITE_STATIC);
@@ -604,7 +604,7 @@ int DBConnector::delete_Jugador(jugador jugadorBorrar)
 		sqlite3_stmt *stmt;
 		char sql[] = "DELETE from Jugadores where NICK = ?";
 
-		std::string nick = jugadorBorrar.nick;
+		std::string nick = jugadorBorrar.getNick();
 	    
 	    //Preparar el statement:
 	    int result = sqlite3_prepare_v2(db, sql, strlen(sql) + 1, &stmt, NULL) ;
@@ -656,7 +656,7 @@ int DBConnector::delete_Pregunta(Preguntas_Respuestas preguntaBorrar)
 		sqlite3_stmt *stmt;
 		char sql[] = "DELETE from Preguntas where ID = ?";
 
-		int ID = preguntaBorrar.ID;
+		int ID = preguntaBorrar.getID();
 	    
 	    //Preparar el statement:
 	    int result = sqlite3_prepare_v2(db, sql, strlen(sql) + 1, &stmt, NULL) ;
@@ -718,7 +718,6 @@ int DBConnector::leer_Jugadores(jugador* listaTodosJugadores)
     //Variables en las que recogeremos los atributos de cada jugador
     int puntuacion;
   	char nick[200];
-  	std::cout << "Antes del Do" << std::endl; 
   
     do
     {
@@ -726,27 +725,14 @@ int DBConnector::leer_Jugadores(jugador* listaTodosJugadores)
       if (result == SQLITE_ROW) 
       {
       	//Leer el nick y la puntuacion de la linea del SELECT
-      	std::cout << "EMPIEZA A LEER" << std::endl; 
        	strcpy(nick, (char*)sqlite3_column_text(stmt, 0));
-       	std::cout << "sTRCPY HECHO" << std::endl; 
       	puntuacion = sqlite3_column_int(stmt, 1);
-
-      	std::cout << "nick: "<< nick << "puntuación " <<  puntuacion << std::endl; 
   
       	//Crear un jugador con esos atributos
-      	jugador jugador;
-      
-      	
-      	jugador.puntuacion = puntuacion;
-      	jugador.nick = nick;
-      	std::cout << "nick: "<< jugador.nick <<  std::endl; 
-      	//Meterlo en la lista
-      	std::cout << "Cont: "<< cont << std::endl; 
+      	jugador jugador = new jugador( nick, puntuacion);
+    
       	listaTodosJugadores[cont] = jugador;
       	cont++;
-
-		std::cout << "Cont: "<< cont << std::endl; 
-  
       }
     } while (result == SQLITE_ROW);
 
@@ -820,39 +806,46 @@ int DBConnector::leer_Preguntas(Pregunta_Respuestas listaTodasPreguntas)
       result = sqlite3_step(stmt);
       if (result == SQLITE_ROW) 
       { 
-      	//Creamos una Pregunta_respuestas con estos atributos:
-      	Preguntas_Respuestas p;
 
       	ID = sqlite3_column_int(stmt, 0);
-      	p.ID = ID;
+      	p.setID (ID);
 
        	strcpy(preguta, (char*)sqlite3_column_text(stmt, 1));   
-       	p.Pregunta = pregunta;
+       	p.setPregunta(pregunta);
 
        	strcpy(r1, (char*)sqlite3_column_text(stmt, 2));
-       	p.Respuesta1 = r1;
+       	p.setRespuesta1 (r1);
 
        	strcpy(r2, (char*)sqlite3_column_text(stmt, 3));
-       	p.Respuesta2 = r2;
+       	p.setRespuesta2 (r2);
       	//Leemos dificultad antes que R3 y R4 ya que depende de este atributo si tiene o no estos dos
       	strcpy(dificultad, (char*)sqlite3_column_text(stmt, 6));
-      	p.Dificultad = dificultad;
+      	p.setDificultad(dificultad);
 
       	if(strcmp(dificultad, "###") == 0)
       	{
       		//Es dificil
       		strcpy(r3, (char*)sqlite3_column_text(stmt, 4));
-      		p.Respuesta3 = r3;
+      		p.SetRespuesta3 ( r3 );
        		strcpy(r4, (char*)sqlite3_column_text(stmt, 5));
-       		p.Respuesta4 = r4;
+       		p.setRespuesta4 (r4);
+
+       		//Creamos una Pregunta_respuestas con estos atributos:
+      		Preguntas_Respuestas p = new (pregunta,r1, r2, r3, r4, dificultad, true);//el ultimo es true para no darle el ID
+      		p.setID (ID);
       	}
       	else if(strcmp(dificultad, "##") == 0)
       	{
       		//Es media
-      		strcpy(r3, (char*)sqlite3_column_text(stmt, 4));
-      		p.Respuesta3 = r3;
+      		p.SetRespuesta3 ( r3 );
+      		Preguntas_Respuestas p = new (pregunta,r1, r2, r3, dificultad, true);//el ultimo es true para no darle el ID
+      		p.setID (ID);
       	}
-      	
+      	else
+      	{
+      		Preguntas_Respuestas p = new (pregunta,r1, r2, dificultad, true);//el ultimo es true para no darle el ID
+      		p.setID (ID);
+      	}
       	listaTodasPreguntas[cont] = p;
       	cont++;  
       }
