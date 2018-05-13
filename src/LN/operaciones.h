14d5155f217fc17a522e0b62f46f2053fa7d3fc1
nagorecb <nagorecb@opendeusto.es>
2018-05-13 17:07:58 +0200

    changes done

--- src/LN/operaciones.h
+++ src/LN/operaciones.h
@@ -1,11 +1,11 @@
 #ifndef OPERACIONES_H
 #define OPERACIONES_H
 
-	#include <preguntas_respuestas.h>
-	#include <prfacil.h>
-	#include <prmedio.h>
-	#include <prdificil.h>
-	#include <jugador.h>
+	#include "preguntas_respuestas.h"
+	#include "prfacil.h"
+	#include "prmedio.h"
+	#include "prdificil.h"
+	#include "jugador.h"
 
 	preguntas_respuestas generarPregunta(const vector<preguntas_respuestas>& arrPreg, vector<preguntas_respuestas>& preguntasSalidas);
 	int comprobarRespuesta(const string& respuesta);
