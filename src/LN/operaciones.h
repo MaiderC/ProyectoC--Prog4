f3a6d09fb7a5db27c555783cb1b01e89a74c723c
HodeiOA <hodei.olaskoaga@opendeusto.es>
2018-05-21 10:41:40 +0200

    operaciones.h again

--- src/LN/operaciones.h
+++ src/LN/operaciones.h
@@ -12,7 +12,7 @@
 
 	using namespace std;
 
-	operaciones::siguienteID(const vector<preguntas_respuestas>& listaTodasRespuestas);
+	void siguienteID(const vector<preguntas_respuestas>& listaTodasRespuestas);
 	preguntas_respuestas generarPregunta(const vector<preguntas_respuestas>& arrPreg, vector<preguntas_respuestas>& preguntasSalidas);
 	int comprobarRespuesta(const string& respuesta);
 	int maxPreguntas(const int cantPreguntas, const int sizeTotalPreguntas);
