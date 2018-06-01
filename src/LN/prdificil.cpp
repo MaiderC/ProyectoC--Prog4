3b68c3f72b2113d9078a295c9aea5ea6e4d8a9f2
maiderc <maiderc@opendeusto.es>
2018-06-01 11:02:03 +0200

    Constructors from prdificil classes updated

--- src/LN/prdificil.cpp
+++ src/LN/prdificil.cpp
@@ -19,14 +19,14 @@ prdificil::prdificil()
   this -> respuesta4 = DEFAULT_RESPUESTA4;
 }
 		
-prdificil::prdificil(string pregunta, string respuesta1, string respuesta2, string respuesta3, string respuesta4, string dificultad, bool leerBD): preguntas_respuestas(pregunta, respuesta1, dificultad, leerBD)
+prdificil::prdificil(string pregunta, string respuesta1, string respuesta2, string respuesta3, string respuesta4, bool leerBD): preguntas_respuestas(pregunta, respuesta1, leerBD)
 {
   // this -> pregunta = pregunta;
   // this -> respuesta1 = respuesta1;
   this -> respuesta2 = respuesta2;
   this -> respuesta3 = respuesta3;
   this -> respuesta4 = respuesta4;
-  // this -> dificultad = dificultad;
+  this -> dificultad = "###";
 }
 
 //destructor
