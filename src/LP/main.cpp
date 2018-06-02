31d4a1ba374f8947f515409e5ad4774eef2debff
maiderc <maiderc@opendeusto.es>
2018-06-02 10:54:26 +0200

    main updated

--- src/LP/main.cpp
+++ src/LP/main.cpp
@@ -208,6 +208,7 @@ int main(int argc, char** argv)
 					if(listaTodasPreguntas.size() == 0)
 			  		{
 			  			mostrarMensaje("No hay preguntas en el sistema :(");
+			  			opcionEliminar = 1;
 			  		} else
 			  		{
 						mostrarTodasPreguntas(listaTodasPreguntas);
